ee2bbf788307f9cd9e4354fc63a5035628fe272e
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-09-29 00:22:45 -0700

    Create coin_model.dart

---
+++ lib/models/coin_model.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class Coin extends Equatable {
+  final String name;
+  final String fullName;
+  final double price;
+
+  const Coin({
+    @required this.name,
+    @required this.fullName,
+    @required this.price,
+  });
+
+  @override
+  List<Object> get props => throw [
+        name,
+        fullName,
+        price,
+      ];
+
+  @override
+  String toString() {
+    return "Coin { $name $fullName - $price }";
+  }
+
+  factory Coin.fromJson(Map<String, dynamic> json) {
+    return Coin(
+      name: json['CoinInfo']['Name'] as String,
+      fullName: json['CoinInfo']['FullName'] as String,
+      price: (json['RAW']['USD']['PRICE'] as num).toDouble(),
+    );
+  }
+}
